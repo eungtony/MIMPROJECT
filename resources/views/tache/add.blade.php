8c62c1f7b52c3cfb0add8c6fe38da331755464d8
Tony <eungtony@outlook.fr>
2016-11-23 22:02:13 +0100

    add categories list

--- resources/views/tache/add.blade.php
+++ resources/views/tache/add.blade.php
@@ -11,32 +11,43 @@
 
                         <a href="{{url()->previous()}}">Retour</a>
 
-                            <form action="{{route('add.tache')}}" method="POST">
+                        <form action="{{route('add.tache')}}" method="POST">
 
-                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                                <input type="hidden" value="{{$idp}}" name="projet_id">
-                                <input type="hidden" value="{{$id}}" name="agence_id">
+                            <input type="hidden" value="{{$idp}}" name="projet_id">
+                            <input type="hidden" value="{{$id}}" name="agence_id">
 
-                                <div class="form-group">
-                                    <input class="form-control" type="text" class="" name="titre" placeholder="Titre de la tâche">
-                                </div>
+                            <div class="form-group">
+                                <input class="form-control" type="text" class="" name="titre"
+                                       placeholder="Titre de la tâche">
+                            </div>
 
-                                <div class="form-group">
-                                    <textarea class="form-control" name="commentaire" placeholder="Commentaire de la tâche"></textarea>
-                                </div>
+                            <div class="form-group">
+                                <textarea class="form-control" name="commentaire"
+                                          placeholder="Commentaire de la tâche"></textarea>
+                            </div>
 
-                                <div class="form-group">
-                                    <label for="">Date</label><br>
-                                    <input type="text" id="datepicker" name="date" class="form-control">
-                                </div>
+                            <div class="form-group">
+                                <label for="">Catégorie de la tâche</label>
+                                <select name="categorie_id" id="" class="form-control">
+                                    @foreach($categories as $categorie)
+                                        <option value="{{$categorie->id}}">{{$categorie->titre}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-primary">
-                                        Ajouter la tâche
-                                    </button>
-                                </div>
-                            </form>
+                            <div class="form-group">
+                                <label for="">Date</label><br>
+                                <input type="text" id="datepicker" name="date" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">
+                                    Ajouter la tâche
+                                </button>
+                            </div>
+                        </form>
 
                     </div>
                 </div>
