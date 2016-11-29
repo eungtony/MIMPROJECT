e002bab577718e9409271a5a890cf2301585f404
Tony <eungtony@outlook.fr>
2016-11-29 15:02:06 +0100

    change col and row

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,89 +32,83 @@
                     </ol>
                 @endif
             </div>
-        </div>
+            @foreach($agences as $agence)
+                <div class="content-panel">
+                    <h1>
+                        <a href="{{route('agence', $agence->id)}}">{{$agence->nom}}</a>
+                    </h1>
 
-        @include('sidebar')
+                    <h3>Projets</h3>
 
-                @foreach($agences as $agence)
+                    @if(!$agence->projets->isEmpty())
 
-            <div class="content-panel">
+                        <table class="table">
+                            <thead>
+                            <th>Nom</th>
+                            <th>Commentaire</th>
+                            <th>Progression du projet</th>
+                            <th>Progression des tâches</th>
+                            </thead>
+                            <tbody>
 
-                        <h1>
-                            <a href="{{route('agence', $agence->id)}}">{{$agence->nom}}</a>
-                        </h1>
+                            @foreach($agence->projets as $projet)
 
-                        <h3>Projets</h3>
+                                <?php
+                                $done = \App\Travail::where('projet_id', $projet->id)->where('fait', 1)->get()->count();
+                                $total = \App\Travail::where('projet_id', $projet->id)->get()->count();
+                                if ($total_etape > 0) {
+                                    $pc_projet = 100 * $projet->etape_id / $total_etape;
+                                }
+                                if ($total > 0) {
+                                    $pc = 100 * $done / $total;
+                                }
+                                ?>
 
-                        @if(!$agence->projets->isEmpty())
-
-                            <table class="table">
-                                <thead>
-                                <th>Nom</th>
-                                <th>Commentaire</th>
-                                <th>Progression du projet</th>
-                                <th>Progression des tâches</th>
-                                </thead>
-                                <tbody>
-
-                                @foreach($agence->projets as $projet)
-
-                                    <?php
-                                    $done = \App\Travail::where('projet_id', $projet->id)->where('fait', 1)->get()->count();
-                                    $total = \App\Travail::where('projet_id', $projet->id)->get()->count();
-                                    if ($total_etape > 0) {
-                                        $pc_projet = 100 * $projet->etape_id / $total_etape;
-                                    }
-                                    if ($total > 0) {
-                                        $pc = 100 * $done / $total;
-                                    }
-                                    ?>
-
-                                    <tr>
-                                        <td>{{$projet->nom}}</td>
-                                        <td>{{$projet->commentaire}}</td>
-                                        <td>
+                                <tr>
+                                    <td>{{$projet->nom}}</td>
+                                    <td>{{$projet->commentaire}}</td>
+                                    <td>
+                                        <div class="progress">
+                                            <div class="progress-bar progress-bar-success progress-bar-striped"
+                                                 role="progressbar" aria-valuenow="{{$pc_projet}}" aria-valuemin="0"
+                                                 aria-valuemax="100" style="width: {{$pc_projet}}%">
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        @if($projet->etape_id > 0)
                                             <div class="progress">
                                                 <div class="progress-bar progress-bar-success progress-bar-striped"
-                                                     role="progressbar" aria-valuenow="{{$pc_projet}}" aria-valuemin="0"
-                                                     aria-valuemax="100" style="width: {{$pc_projet}}%">
+                                                     role="progressbar" aria-valuenow="{{$pc}}" aria-valuemin="0"
+                                                     aria-valuemax="100" style="width: {{$pc}}%">
                                                 </div>
                                             </div>
-                                        </td>
-                                        <td>
-                                            @if($projet->etape_id > 0)
-                                                <div class="progress">
-                                                    <div class="progress-bar progress-bar-success progress-bar-striped"
-                                                         role="progressbar" aria-valuenow="{{$pc}}" aria-valuemin="0"
-                                                         aria-valuemax="100" style="width: {{$pc}}%">
-                                                    </div>
+                                        @else
+                                            <div class="progress">
+                                                <div class="progress-bar progress-bar-danger progress-bar-striped"
+                                                     role="progressbar" aria-valuenow="100" aria-valuemin="0"
+                                                     aria-valuemax="100" style="width: {{$pc}}%">
                                                 </div>
-                                            @else
-                                                <div class="progress">
-                                                    <div class="progress-bar progress-bar-danger progress-bar-striped"
-                                                         role="progressbar" aria-valuenow="100" aria-valuemin="0"
-                                                         aria-valuemax="100" style="width: {{$pc}}%">
-                                                    </div>
-                                                </div>
-                                            @endif
-                                        </td>
-                                    </tr>
+                                            </div>
+                                        @endif
+                                    </td>
+                                </tr>
 
-                                @endforeach
+                            @endforeach
 
-                                @else
+                            @else
 
-                                    <p class="bg-danger">
-                                        Cette agence ne possède pas de projets !
-                                    </p>
+                                <p class="bg-danger">
+                                    Cette agence ne possède pas de projets !
+                                </p>
 
-                                @endif
+                            @endif
 
-                                </tbody>
-                            </table>
-
-                    </div>
-
-                @endforeach
+                            </tbody>
+                        </table>
+                </div>
+            @endforeach
+        </div>
+        @include('sidebar')
     </div>
 @endsection
