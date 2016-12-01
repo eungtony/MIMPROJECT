a0fbed5dde23904b61f46fd3a0d8abfa07319ea6
Tony <eungtony@outlook.fr>
2016-12-02 00:28:32 +0100

    collapse edit file and add

--- resources/views/projet/index.blade.php
+++ resources/views/projet/index.blade.php
@@ -62,17 +62,12 @@ if ($total > 0) {
                                    data-confirm="Voulez-vous supprimer ce fichier ?">Supprimer</a>
                         @endif
                     @endforeach
-
                     <hr>
-
                 @else
-
                     <p class="bg-danger">
                         Aucun fichier présent.
                     </p>
-
                 @endif
-
                 @if($user_id == $cdp_id || $statut_id == $ca_id)
                     <a href="#upload" class="btn btn-primary" data-toggle="collapse" data-target="#upload">Téléverser un
                         fichier</a>
@@ -97,9 +92,7 @@ if ($total > 0) {
                         </form>
                     </div>
                 @endif
-
                 <hr>
-
                 <h3>Progression du projet</h3>
                 <div class="progress">
                     <div class="progress-bar progress-bar-success progress-bar-striped"
@@ -205,53 +198,51 @@ if ($total > 0) {
                         </tr>
 
                         @if($user_id == $cdp_id || $statut_id == $ca_id)
-                            <tr>
-                                <td>
-                                    <div class="collapse" id="tache{{$tache->id}}">
-                                        <div class="well">
-                                            <form action="{{route('edit.tache', [$tache->id, $tache->projet_id])}}"
-                                                  method="POST">
-                                                <input type="hidden" name="_token"
-                                                       value="{{ csrf_token() }}">
+                            <div class="collapse" id="tache{{$tache->id}}">
+                                <div class="well">
+                                    <form action="{{route('edit.tache', [$tache->id, $tache->projet_id])}}"
+                                          method="POST">
+                                        <input type="hidden" name="_token"
+                                               value="{{ csrf_token() }}">
 
-                                                <div class="form-group">
-                                                    <input class="form-control" type="text" name="titre"
-                                                           value="{{$tache->titre}}">
-                                                </div>
-                                                <div class="form-group">
+                                        <div class="form-group">
+                                            <input class="form-control" type="text" name="titre"
+                                                   value="{{$tache->titre}}">
+                                        </div>
+                                        <div class="form-group">
                                                         <textarea class="form-control" name="commentaire" id=""
                                                                   cols="30" rows="2">{{$tache->commentaire}}</textarea>
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="">Date</label><br>
-                                                    <input type="text" id="datepicker" name="date"
-                                                           value="{{$tache->date}}" class="form-control">
-                                                </div>
-                                                <label for="" class="checkbox-inline">
-                                                    <input type="checkbox" name="fait" id="fait"
-                                                           value="{{$tache->fait}}"
-                                                           @if($tache->fait == 1) checked @endif>
-                                                    Etat de la tâche
-                                                </label>
-                                                <div class="form-group">
-                                                    <select name="user_id" class="form-control">
-                                                        @foreach($users as $u)
-                                                            <option value="{{$u->id}}"
-                                                                    @if($u->id == $tache->user_id) selected @endif >{{$u->name}}</option>
-                                                        @endforeach
-                                                    </select>
-                                                </div>
-                                                <hr>
-                                                <div class="form-group">
-                                                    <button class="btn btn-success" type="submit">
-                                                        Modifier cette tâche
-                                                    </button>
-                                                </div>
-                                            </form>
                                         </div>
-                                    </div>
-                                </td>
-                            </tr>
+                                        <div class="form-group">
+                                            <label for="">Date</label><br>
+                                            <input type="text" id="datepicker" name="date"
+                                                   value="{{$tache->date}}" class="form-control">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="" class="checkbox-inline">
+                                                <input type="checkbox" name="fait" id="fait"
+                                                       value="{{$tache->fait}}"
+                                                       @if($tache->fait == 1) checked @endif>
+                                                Etat de la tâche
+                                            </label>
+                                        </div>
+                                        <div class="form-group">
+                                            <select name="user_id" class="form-control">
+                                                @foreach($users as $u)
+                                                    <option value="{{$u->id}}"
+                                                            @if($u->id == $tache->user_id) selected @endif >{{$u->name}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <hr>
+                                        <div class="form-group">
+                                            <button class="btn btn-success" type="submit">
+                                                Modifier cette tâche
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
                         @endif
                     @endforeach
                     </tbody>
