fd9eced934a0ec89b6b3a62ee01e656ae31cae82
Tony <eungtony@outlook.fr>
2016-12-02 10:45:09 +0100

    add row and col

--- resources/views/flash.blade.php
+++ resources/views/flash.blade.php
@@ -1,23 +1,27 @@
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-            @foreach ($errors->all() as $error)
-                <p>{{ $error }}</p>
-            @endforeach
+<div class="row mt">
+    <div class="col-md-12">
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            </div>
+        @endif
+
+        @if(session()->has('success'))
+
+            <div class="alert alert-success" style="width: 80%; margin:auto;">
+                {{ session('success') }}
+            </div>
+
+        @endif
+
+        @if(session()->has('error'))
+
+            <div class="alert alert-danger" style="width: 80%; margin:auto;">
+                {{ session('error') }}
+            </div>
+
+        @endif
     </div>
-@endif
-
-@if(session()->has('success'))
-
-    <div class="alert alert-success" style="width: 80%; margin:auto;">
-        {{ session('success') }}
-    </div>
-
-@endif
-
-@if(session()->has('error'))
-
-    <div class="alert alert-danger" style="width: 80%; margin:auto;">
-        {{ session('error') }}
-    </div>
-
-@endif
+</div>
