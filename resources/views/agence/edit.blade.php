950fff052616c2640cf027085da2423fd7c4ad16
Tony <eungtony@outlook.fr>
2016-12-03 23:24:20 +0100

    remove choice of head project

--- resources/views/agence/edit.blade.php
+++ resources/views/agence/edit.blade.php
@@ -5,19 +5,8 @@
             <input class="form-control" type="text" name="nom" value="{{$agence->nom}}">
         </div>
 
-        <h4>Sélectionnez le chef de l'agence</h4>
-
         <div class="form-group">
-            <select name="user_id" class="form-control" value="{{$agence->user_id}}">
-                @foreach($users as $user)
-                    <option value="{{$user->id}}"
-                            @if($user->id == $cdp_id) selected @endif>{{$user->name}}</option>
-                @endforeach
-            </select>
-        </div>
-
-        <div class="form-group">
-            <button class="btn btn-primary" type="submit">Modifier l'agence</button>
+            <button class="btn btn-primary" type="submit">Modifier le nom de l'agence</button>
         </div>
 
     </form>
