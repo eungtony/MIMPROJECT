fe1e1d2c571df377d74270b07da7a69c20986001
Tony <eungtony@outlook.fr>
2016-12-03 23:25:19 +0100

    add inline form

--- resources/views/projet/file.blade.php
+++ resources/views/projet/file.blade.php
@@ -1,9 +1,11 @@
-<form action="{{route('file.edit.projet', [$projet->agence_id,$projet->id,$file->id])}}"
+<form action="{{route('file.edit.projet', [$projet->agence_id,$projet->id,$file->id])}}" class="form-inline"
       method="post">
     {{csrf_field()}}
     <div class="form-group">
         <input class="form-control" type="text" name="titre"
                value="{{$file->titre}}">
     </div>
-    <button class="btn btn-primary">Modifier</button>
+    <div class="form-group">
+        <button class="btn btn-primary">Modifier</button>
+    </div>
 </form>
