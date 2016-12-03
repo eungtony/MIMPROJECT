cc1a1049b4d02caf232b054ec7cc3b23d61e12d1
Tony <eungtony@outlook.fr>
2016-12-03 14:34:12 +0100

    apply application layout and put avatar

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,18 +1,22 @@
-@extends('layouts.app')
+@extends('layouts.application')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Welcome</div>
+    <div class="row mt">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">{{$user->name}}</div>
 
-                    <div class="panel-body">
+                <div class="panel-body">
+                    @if($user->avatar != 0)
+                        <img src="{{asset('/avatars/'.$user->id.'.'.$user->extension)}}" class="img-circle" width="100"
+                             alt="">
+                    @else
+                        <img src="{{asset('/avatars/user.png')}}" class="img-circle" width="100"
+                             alt="">
+                    @endif
                         <h1>{{$user->name}}</h1>
                         <h2>Appartient à l'agence {{$user->agence->nom}}</h2>
                         <h3>{{$user->poste->nom}}</h3>
-                        <h3>{{$user->statut->titre}}</h3>
-                    </div>
                 </div>
             </div>
         </div>
