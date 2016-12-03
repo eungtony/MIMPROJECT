ae35b5e762ca7c8721f388834a0905bf81e13986
Tony <eungtony@outlook.fr>
2016-12-03 23:43:55 +0100

    change as include file

--- resources/views/tache/index.blade.php
+++ resources/views/tache/index.blade.php
@@ -1,31 +1,15 @@
-<?php
-$user_id = Auth::user()->id;
-$statut_id = Auth::user()->statut_id;
-$ca_id = 1;
-$b_id = 2;
-?>
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">{{$taches->titre}}</div>
-
-                    <div class="panel-body">
-
-                        <a href="{{url()->previous()}}">Retour</a>
-
-                        <h1>{{$taches->titre}}</h1>
-                        <p>{{$taches->commentaire}}</p>
-                        <p class="text-right">{{$taches->date}}</p>
-                        <p class="text-right">{{$taches->categorie->titre}}</p>
-                        <p class="text-right">{{$taches->user->name}}</p>
-
-                    </div>
-                </div>
+<div class="modal fade" id="tache{{$tache->id}}" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                {{$tache->titre}}
+            </div>
+            <div class="modal-body">
+                <p>{{$tache->commentaire}}</p>
+                <p class="text-right">{{$tache->date}}</p>
+                <p class="text-right">{{$tache->categorie->titre}}</p>
+                <p class="text-right">{{$tache->user->name}}</p>
             </div>
         </div>
     </div>
-@endsection
+</div>
