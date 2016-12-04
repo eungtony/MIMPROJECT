33ea9d9c9dcc9e2bd47ccf87b4bedc4e7c4bfc58
Tony <eungtony@outlook.fr>
2016-12-04 15:09:15 +0100

    minor spelling changes

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -2,30 +2,30 @@
     <div class="col-md-3 col-sm-2 box0">
         <div class="box1">
             <span class="li_cloud"></span>
-            <h3>{{$agence->file->count()}} fichiers</h3>
+            <h3>{{$agence->file->count()}} fichier(s)</h3>
         </div>
-        <p>{{$agence->file->count()}} fichiers dans votre agence</p>
+        <p>{{$agence->file->count()}} fichier(s) dans votre agence</p>
     </div>
     <div class="col-md-3 col-sm-2 box0">
         <div class="box1">
             <span class="li_news"></span>
-            <h3>{{$messages->count()}} messages</h3>
+            <h3>{{$messages->count()}} message(s)</h3>
         </div>
-        <p>Vous avez {{$messages->count()}} messages dans votre agence</p>
+        <p>Vous avez {{$messages->count()}} message(s) dans votre agence</p>
     </div>
     <div class="col-md-3 col-sm-2 box0">
         <div class="box1">
             <span class="li_banknote"></span>
             <h3>{{$bankable}} €</h3>
         </div>
-        <p>Vous avez récolté {{$bankable}} €</p>
+        <p>Vous avez récolté {{$bankable}} euros</p>
     </div>
     <div class="col-md-3 col-sm-2 box0">
         <div class="box1">
             <span class="li_data"></span>
-            <h3>{{$projets->count()}} projets</h3>
+            <h3>{{$projets->count()}} projet(s)</h3>
         </div>
-        <p>Vous avez {{$projets->count()}} projets en cours</p>
+        <p>Vous avez {{$projets->count()}} projet(s) en cours</p>
     </div>
 
 </div><!-- /row mt -->
