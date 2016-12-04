ff5585894e750fe34d53c20e32355f29260cb2af
Tony <eungtony@outlook.fr>
2016-12-04 19:55:03 +0100

    change agency id to load agency from url parameter

--- resources/views/agence/index.blade.php
+++ resources/views/agence/index.blade.php
@@ -2,7 +2,7 @@
 $user_id = Auth::user()->id;
 $statut_id = Auth::user()->statut_id;
 $ca_id = 1;
-$projets = \App\Projet::where('agence_id', \Illuminate\Support\Facades\Auth::user()->agence_id)->take(5)->get();
+$projets = \App\Projet::where('agence_id', $id)->take(5)->get();
 ?>
 @extends('layouts.application')
 
