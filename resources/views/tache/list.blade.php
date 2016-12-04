e888f4a4977366fd16fe4283b18034d3144ef105
Tony <eungtony@outlook.fr>
2016-12-04 19:55:59 +0100

    create task list file to include

---
+++ resources/views/tache/list.blade.php
@@ -0,0 +1,65 @@
+<div class="row mt">
+    <div class="col-md-12">
+        <section class="task-panel tasks-widget">
+            <div class="panel-heading">
+                <div class="pull-left">
+                    <h5>
+                        <i class="fa fa-tasks"></i>
+                        Tâches ({{$done}}/{{$total}})
+                        @if($user_id == $cdp_id || $statut_id == $ca_id)
+                            <a href="#addtask{{$projet->id}}" data-toggle="modal"
+                               data-target="#addtask{{$projet->id}}"
+                               class="btn btn-warning btn-xs">Ajouter une tache</a>
+                            @include('tache.add')
+                        @endif
+                    </h5>
+                </div>
+                <br>
+            </div>
+            <div class="panel-body">
+                <div class="task-content">
+                    @foreach($taches as $tache)
+                        <ul class="task-list" style="margin-bottom: 20px;">
+                            <li>
+                                <div class="task-title">
+                                    <span class="task-title-sp">{{$tache->titre}}</span>
+                                    <span class="badge bg-theme">A faire</span>
+                                                        <span class="badge bg-success">
+                                                            @if($tache->user)
+                                                                {{$tache->user->name}}
+                                                            @else
+                                                                Aucune personne assignée
+                                                            @endif
+                                                        </span>
+                                                        <span class="badge bg-important">
+                                                            {{$tache->categorie->titre}}
+                                                        </span>
+                                                        <span class="badge bg-danger">
+                                                            {{$tache->date}}
+                                                        </span>
+                                    <div class="pull-right hidden-phone">
+                                        <button class="btn btn-success btn-xs"><i
+                                                    class=" fa fa-check"></i></button>
+                                        <a href="#tache{{$tache->id}}"
+                                           class="btn btn-primary btn-xs"
+                                           data-toggle="modal"
+                                           aria-controls="#tache{{$tache->id}}"><i
+                                                    class="fa fa-pencil"></i></a>
+                                        <a href="{{action('tacheController@destroy', $tache->id)}}"
+                                           data-method="delete"
+                                           data-confirm="Souhaitez-vous réellement supprimer cette tâche ?"
+                                           class="btn btn-danger btn-xs"><i
+                                                    class="fa fa-trash-o "></i></a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        @if($user_id == $cdp_id || $statut_id == $ca_id)
+                            @include('tache.edit')
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+        </section>
+    </div><!-- /col-md-12-->
+</div><!-- /row -->
