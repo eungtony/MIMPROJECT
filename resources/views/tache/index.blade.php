72807e991816eab8affe892ce5c7d5913b3a9d10
Tony <eungtony@outlook.fr>
2016-12-04 15:50:00 +0100

    change div id

--- resources/views/tache/index.blade.php
+++ resources/views/tache/index.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="tache{{$tache->id}}" role="dialog">
+<div class="modal fade" id="voirtache{{$tache->id}}" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
