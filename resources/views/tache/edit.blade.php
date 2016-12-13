b1b4bd43a5c61c4cda2ce990acb8cd2f37774d1f
Tony <eungtony@outlook.fr>
2016-12-13 09:54:38 +0100

    add categories and date input to edit task

--- resources/views/tache/edit.blade.php
+++ resources/views/tache/edit.blade.php
@@ -16,24 +16,39 @@
                                value="{{$tache->titre}}">
                     </div>
                     <div class="form-group">
-                        <textarea class="form-control" name="commentaire" id="" cols="30" rows="10">
+                        <textarea class="form-control" name="commentaire" id="" cols="30" rows="7">
                             {{$tache->commentaire}}
                         </textarea>
                     </div>
-                    <label for="" class="checkbox-inline">
-                        <input type="checkbox" name="fait"
-                               value="{{$tache->fait}}"
-                               @if($tache->fait == 1) checked @endif>
-                        Etat de la tâche
-                    </label>
-                    <hr>
                     <div class="form-group">
+                        <select name="user_id" class="form-control">
+                            @foreach($categories as $categorie)
+                                <option
+                                        @if($categorie->id == $tache->categorie_id) selected @endif
+                                value="{{$categorie->id}}" id="">{{$categorie->titre}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="" @if($tache->user_id == 0) style="color:red;" @endif >Assignez une personne</label>
                         <select name="user_id" class="form-control">
                             @foreach($users as $u)
                                 <option value="{{$u->id}}"
                                         @if($u->id == $tache->user_id) selected @endif >{{$u->name}}</option>
                             @endforeach
                         </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="checkbox-inline">
+                            <input type="checkbox" name="fait"
+                                   value="{{$tache->fait}}"
+                                   @if($tache->fait == 1) checked @endif>
+                            Etat de la tâche
+                        </label>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Date</label><br>
+                        <input id="datepicker2" name="date" value="{{$tache->date}}" class="form-control">
                     </div>
                     <div class="form-group">
                         <button class="btn btn-success"
@@ -46,3 +61,8 @@
         </div>
     </div>
 </div>
+<script>
+    $(function () {
+        $("#datepicker2").datepicker({dateFormat: 'yy-mm-dd'});
+    });
+</script>
