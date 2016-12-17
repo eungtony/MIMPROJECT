4b2429695644f3d3003ad8abf66d293305be0c68
Tony <eungtony@outlook.fr>
2016-12-17 17:13:55 +0100

    surround form by div collapse

--- resources/views/projet/file.blade.php
+++ resources/views/projet/file.blade.php
@@ -1,16 +1,23 @@
-<form action="{{route('file.edit.projet', [$projet->agence_id,$projet->id,$file->id])}}" class="form-inline"
-      method="post">
-    {{csrf_field()}}
-    <div class="form-group">
-        <input class="form-control" type="text" name="titre"
-               value="{{$file->titre}}">
+<div class="collapse" id="editFile{{$file->id}}">
+    <div class="row mt">
+        <div class="col-md-12">
+            <form action="{{route('file.edit.projet', [$projet->agence_id,$projet->id,$file->id])}}" class="form-inline"
+                  method="post">
+                {{csrf_field()}}
+                <div class="form-group">
+                    <input class="form-control" type="text" name="titre"
+                           value="{{$file->titre}}">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary">Modifier</button>
+                </div>
+                <a href="{{route('file.delete.projet', [$file->agence_id,$file->projet_id,$file->id])}}"
+                   class="btn btn-danger"
+                   data-method="delete"
+                   data-confirm="Voulez-vous supprimer ce fichier ?">
+                    <i class="fa fa-trash-o "></i>
+                </a>
+            </form>
+        </div>
     </div>
-    <div class="form-group">
-        <button class="btn btn-primary">Modifier</button>
-    </div>
-    <a href="{{route('file.delete.projet', [$file->agence_id,$file->projet_id,$file->id])}}" class="btn btn-danger"
-       data-method="delete"
-       data-confirm="Voulez-vous supprimer ce fichier ?">
-        <i class="fa fa-trash-o "></i>
-    </a>
-</form>
+</div>
