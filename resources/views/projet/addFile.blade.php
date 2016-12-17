b525cb8f50d0ff174d90f506ec952f8f02475072
Tony <eungtony@outlook.fr>
2016-12-17 16:13:34 +0100

    create add file view for project

---
+++ resources/views/projet/addFile.blade.php
@@ -0,0 +1,21 @@
+<div class="collapse" id="file">
+    <hr>
+    <form action="{{route('file.projet', [$projet->agence_id, $projet->id])}}" class="form-inline"
+          enctype="multipart/form-data"
+          method="POST">
+        {{csrf_field()}}
+        <div class="form-group">
+            <label for="">Nommer votre fichier</label>
+            <input class="form-control" type="text" name="titre">
+        </div>
+        <div class="form-group">
+            <label for="">Téleverser un fichier</label>
+            <input type="file" name="file" class="form-control">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">
+                <i class="fa fa-upload" aria-hidden="true"></i>
+            </button>
+        </div>
+    </form>
+</div>
