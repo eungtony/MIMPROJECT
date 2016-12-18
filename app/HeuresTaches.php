a2247437b8984cfb593d9ccdd9ba5028c4e3656e
Tony <eungtony@outlook.fr>
2016-12-18 16:00:57 +0100

    create new model

--- app/HeuresTaches.php
+++ app/HeuresTaches.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class HeuresTaches extends Model
 {
-    //
+    protected $fillable = ['heures', 'description', 'user_id', 'tache_id', 'agence_id', 'projet_id'];
 }
