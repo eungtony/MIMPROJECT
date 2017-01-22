cb9bd2c210e1607c39f86d6bc33b23467c8a1349
Tony <eungtony@outlook.fr>
2017-01-22 23:32:50 +0100

    message if there are no projects

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -144,75 +144,19 @@ $projets = \App\Projet::where('agence_id', 0)->get();
             @endforeach -->
 
             <h3>PROJETS LIBRES</h3>
-            @foreach($projets as $project)
-                <?php
-                $propose = \App\Projet_agence::where('projet_id', $project->id)->where('agence_id', $agence_id)->get();
-                $propose_id = 0;
-                if (!$propose->isEmpty()) {
-                    $propose_id = $propose[0]->id;
-                }
-                ?>
-                <div class="desc">
-                    <div class="details" style="padding:10px;">
-                        <a href="#project{{$project->id}}" data-toggle="modal">{{$project->nom}}</a>
-                        <p>{{substr($project->commentaire, 0, 50)}}</p>
-                        @if($user_id == $cdp_id)
-                            @if($propose->isEmpty())
-                                <form action="{{route('add.projet.agence', [$project->id, Auth::user()->agence_id])}}"
-                                      method="POST">
-                                    {{csrf_field()}}
-                                    <div class="form-group text-right">
-                                        <button type="submit" class="btn btn-success btn-xs">
-                                            Proposer mon agence !
-                                        </button>
-                                    </div>
-                                </form>
-                            @else
-                                <div class="text-right">
-                                    <a
-                                            href="{{route('delete.projet.agence', $propose_id)}}"
-                                            data-method="delete"
-                                            data-confirm="Souhaitez-vous réellement vous désistez ?"
-                                            class="btn btn-danger btn-xs"
-                                            style="color:white;"
-                                    >
-                                        Me désister
-                                    </a>
-                                </div>
-                            @endif
-                        @endif
-                    </div>
-                </div>
-
-                <div class="modal fade" id="project{{$project->id}}" role="dialog">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                {{$project->nom}}
-                                @if($statut_id == $bureau_id || $statut_id == $ca_id)
-                                    <a href="#editProject{{$project->id}}" data-toggle="collapse"
-                                       class="btn btn-primary btn-xs">Modifier</a>
-                                @endif
-                            </div>
-                            <div class="modal-body">
-                                {{$project->commentaire}}
-                                <h4>Liste des agences qui se sont proposées</h4>
-                                <?php
-                                $propose_projet = \App\Projet_agence::where('projet_id', $project->id)->get();
-                                ?>
-                                @if(!$propose_projet->isEmpty())
-                                    <ul>
-                                        @foreach($propose_projet as $agency)
-                                            <li>
-                                                {{$agency->nom_agence}}
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                @else
-                                    <div class="alert alert-warning">
-                                        Aucune agence ne s'est proposée !
-                                    </div>
-                                @endif
+                @if(!$projets->isEmpty())
+                    @foreach($projets as $project)
+                        <?php
+                        $propose = \App\Projet_agence::where('projet_id', $project->id)->where('agence_id', $agence_id)->get();
+                        $propose_id = 0;
+                        if (!$propose->isEmpty()) {
+                            $propose_id = $propose[0]->id;
+                        }
+                        ?>
+                        <div class="desc">
+                            <div class="details" style="padding:10px;">
+                                <a href="#project{{$project->id}}" data-toggle="modal">{{$project->nom}}</a>
+                                <p>{{substr($project->commentaire, 0, 50)}}</p>
                                 @if($user_id == $cdp_id)
                                     @if($propose->isEmpty())
                                         <form action="{{route('add.projet.agence', [$project->id, Auth::user()->agence_id])}}"
@@ -235,50 +179,113 @@ $projets = \App\Projet::where('agence_id', 0)->get();
                                             >
                                                 Me désister
                                             </a>
+                                    </div>
+                                    @endif
+                            @endif
+                            </div>
+                    </div>
+
+                        <div class="modal fade" id="project{{$project->id}}" role="dialog">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        {{$project->nom}}
+                                        @if($statut_id == $bureau_id || $statut_id == $ca_id)
+                                            <a href="#editProject{{$project->id}}" data-toggle="collapse"
+                                               class="btn btn-primary btn-xs">Modifier</a>
+                                        @endif
+                                    </div>
+                                    <div class="modal-body">
+                                        {{$project->commentaire}}
+                                        <h4>Liste des agences qui se sont proposées</h4>
+                                        <?php
+                                        $propose_projet = \App\Projet_agence::where('projet_id', $project->id)->get();
+                                        ?>
+                                        @if(!$propose_projet->isEmpty())
+                                            <ul>
+                                                @foreach($propose_projet as $agency)
+                                                    <li>
+                                                        {{$agency->nom_agence}}
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                    @else
+                                            <div class="alert alert-warning">
+                                                Aucune agence ne s'est proposée !
                                         </div>
                                     @endif
-                                @endif
-                                @if($statut_id == $bureau_id || $statut_id == $ca_id)
-                                    <h4>Attribuez ce projet à une agence</h4>
-                                    <form action="{{route('attribute.project', [$project->id])}}" method="POST">
-                                        {{csrf_field()}}
-                                        <div class="form-group">
-                                            <select class="form-control" name="agence_id">
-                                                @foreach($propose_projet as $agency)
-                                                    <option value="{{$agency->agence_id}}">{{$agency->nom_agence}}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                        <div class="form-group">
-                                            <button type="submit" class="btn btn-primary">
-                                                Attribuez le projet
-                                            </button>
-                                        </div>
-                                    </form>
-                                    <div class="collapse" id="editProject{{$project->id}}">
-                                        <hr>
-                                        <form action="{{route('edit.free.project', [$project->id])}}" method="POST">
+                                        @if($user_id == $cdp_id)
+                                            @if($propose->isEmpty())
+                                                <form action="{{route('add.projet.agence', [$project->id, Auth::user()->agence_id])}}"
+                                                      method="POST">
+                                                    {{csrf_field()}}
+                                                    <div class="form-group text-right">
+                                                        <button type="submit" class="btn btn-success btn-xs">
+                                                            Proposer mon agence !
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            @else
+                                                <div class="text-right">
+                                                    <a
+                                                            href="{{route('delete.projet.agence', $propose_id)}}"
+                                                            data-method="delete"
+                                                            data-confirm="Souhaitez-vous réellement vous désistez ?"
+                                                            class="btn btn-danger btn-xs"
+                                                            style="color:white;"
+                                                    >
+                                                        Me désister
+                                                    </a>
+                                                </div>
+                                            @endif
+                                        @endif
+                                        @if($statut_id == $bureau_id || $statut_id == $ca_id)
+                                            <h4>Attribuez ce projet à une agence</h4>
+                                            <form action="{{route('attribute.project', [$project->id])}}" method="POST">
                                             {{csrf_field()}}
                                             <div class="form-group">
-                                                <input type="text" class="form-control" name="nom"
-                                                       value="{{$project->nom}}">
+                                                <select class="form-control" name="agence_id">
+                                                    @foreach($propose_projet as $agency)
+                                                        <option value="{{$agency->agence_id}}">{{$agency->nom_agence}}</option>
+                                                    @endforeach
+                                                </select>
                                             </div>
                                             <div class="form-group">
-                                            <textarea name="commentaire" class="form-control" cols="30" rows="10">
-                                                {{$project->commentaire}}
-                                            </textarea>
-                                            </div>
-                                            <div class="form-group">
-                                                <button class="btn btn-primary" type="submit">
-                                                    Modifier le projet
+                                                <button type="submit" class="btn btn-primary">
+                                                    Attribuez le projet
                                                 </button>
                                             </div>
                                         </form>
+                                            <div class="collapse" id="editProject{{$project->id}}">
+                                                <hr>
+                                                <form action="{{route('edit.free.project', [$project->id])}}"
+                                                      method="POST">
+                                                    {{csrf_field()}}
+                                                    <div class="form-group">
+                                                        <input type="text" class="form-control" name="nom"
+                                                               value="{{$project->nom}}">
+                                                    </div>
+                                                    <div class="form-group">
+                                            <textarea name="commentaire" class="form-control" cols="30" rows="10">
+                                                {{$project->commentaire}}
+                                            </textarea>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <button class="btn btn-primary" type="submit">
+                                                            Modifier le projet
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        @endif
                                     </div>
-                                @endif
                             </div>
                         </div>
                     </div>
+                    @endforeach
+                @else
+                    <div class="alert alert-warning">
+                        Aucun projet proposé
                 </div>
-            @endforeach
+                @endif
 </div><!-- /col-lg-3 -->
