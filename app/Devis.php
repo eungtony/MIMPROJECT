e0173a6b0aa9dce058053191f61290965d6cf6b0
Tony <eungtony@outlook.fr>
2017-01-22 20:27:02 +0100

    add project relationship

--- app/Devis.php
+++ app/Devis.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Devis extends Model
 {
-    protected $fillable = ['agence_id', 'projet_id', 'user_id', 'valide'];
+    protected $fillable = ['agence_id', 'projet_id', 'user_id', 'valide', 'a_valider'];
+
+    public function projet()
+    {
+        return $this->belongsTo('App\Projet');
+    }
 }
