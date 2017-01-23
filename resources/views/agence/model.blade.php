67d682991671e1448a7f890a8e27bece85a48208
cedricnoel <cedric10troyes@gmail.com>
2017-01-23 10:29:49 +0100

    Fixed model bug

--- resources/views/agence/model.blade.php
+++ resources/views/agence/model.blade.php
@@ -128,27 +128,28 @@
                 </p>
             @else
                 @foreach($projets as $projet)
-                    <?php
-                    $taches = \App\Travail::where('projet_id', $projet->id)->orderBy('id', 'desc')->get();
-                    if (request()->only('sort')['sort'] == 'date') {
-                        $taches = \App\Travail::where('projet_id', $projet->id)->orderBy('date', 'asc')->get();
-                    } elseif (request()->only('sort')['sort'] == 'category') {
-                        $taches = \App\Travail::where('projet_id', $projet->id)->orderBy('categorie_id', 'asc')->get();
-                    } elseif (request()->only('sort')['sort'] == 'done') {
-                        $taches = \App\Travail::where('projet_id', $projet->id)->orderBy('fait', 'desc')->get();
-                    }
-                    $taches->load('user');
-                    $users = \App\User::where('agence_id', $projet->agence_id)->get();
-                    $done = \App\Travail::where('projet_id', $projet->id)->where('fait', 1)->get()->count();
-                    $total = \App\Travail::where('projet_id', $projet->id)->get()->count();
-                    $projet_heures = \App\HeuresTaches::where('projet_id', $projet->id)->get();
-                    $heures_notees = 0;
-                    foreach ($projet_heures as $heure) {
-                        $heures_notees = $heures_notees + $heure->heures;
-                    }
-                    $pc = 0;
-                    $pc_projet = 0;
-                    $heures = 0;
+                        <?php
+                        $taches = \App\Travail::where('projet_id', $projet->id)->orderBy('id', 'desc')->get();
+                        if (request()->only('sort')['sort'] == 'date') {
+                            $taches = \App\Travail::where('projet_id', $projet->id)->orderBy('date', 'asc')->get();
+                        } elseif (request()->only('sort')['sort'] == 'category') {
+                            $taches = \App\Travail::where('projet_id', $projet->id)->orderBy('categorie_id', 'asc')->get();
+                        } elseif (request()->only('sort')['sort'] == 'done') {
+                            $taches = \App\Travail::where('projet_id', $projet->id)->orderBy('fait', 'desc')->get();
+                        }
+                        $taches->load('user');
+                        $users = \App\User::where('agence_id', $projet->agence_id)->get();
+                        $done = \App\Travail::where('projet_id', $projet->id)->where('fait', 1)->get()->count();
+                        $total = \App\Travail::where('projet_id', $projet->id)->get()->count();
+                        $projet_heures = \App\HeuresTaches::where('projet_id', $projet->id)->get();
+                        $heures_notees = 0;
+                        foreach ($projet_heures as $heure) {
+                            $heures_notees = $heures_notees + $heure->heures;
+                        }
+                        $pc = 0;
+                        $pc_projet = 0;
+                        $heures = 0;
+
                         if ($total_etape > 0) {
                             $pc_projet = 100 * $projet->etape_id / $total_etape;
                         }
@@ -160,82 +161,66 @@
                             $heures = 100 * $heures_notees / $projet->total_heures;
                         }
 
-                    $etape = "Le projet n'a pas encore commencé";
-                    if ($projet->etape_id > 0) {
-                        $etape = \App\Etape::findOrFail($projet->etape_id);
-                    }
-                    ?>
-                <!-- TABLEAU PROJETS -->
-                    <div style="margin-bottom: 15px;">
-                        <div class="content-panel">
-                            <div class="text-center">
-                                <h3 style="display: inline-block;">
-                                    <a href="#pr{{$projet->id}}"
-                                       data-toggle="collapse" aria-expanded="false"
-                                       aria-controls="#pr{{$projet->id}}">
-                                        {{ $projet->nom }}
-                                    </a>
-                                </h3>
-                                <a href="{{route('projet', [$projet->agence_id, $projet->id])}}"
-                                   class="btn btn-success btn-xs"
-                                   style="margin-bottom: 15px;margin-left: 20px;">Détail du projet</a>
-                                @if($user_id == $cdp_id || $statut_id == $ca_id)
-                                    <a class="btn btn-primary btn-xs" style="margin-bottom: 15px;margin-left: 20px;"
-                                       href="#edit{{$projet->id}}" data-toggle="modal"
-                                       aria-controls="#edit{{$projet->id}}">
-                                        <i class="fa fa-pencil"></i>
-                                    </a>
-                                    <a class="btn btn-danger btn-xs" style="margin-bottom: 15px;"
-                                       href="{{ route('projet.destroy', [$agence->id, $projet->id]) }}"
-                                       data-method="delete"
-                                       data-confirm="Voulez-vous réellement supprimer ce projet ?">
-                                        <i class="fa fa-trash-o "></i>
-                                    </a>
-                                    <a class="btn btn-success btn-xs"
-                                       href="{{route('projet', [$agence->id, $projet->id])}}#devis"
-                                       style="margin-bottom:15px;">
-                                        <i class="fa fa-money"></i>
-                                    </a>
-                                    @include('projet.edit')
-                                @endif
-                                <span style="margin-left:30px;">
-                        <a href="#task{{$projet->id}}"
-                           data-toggle="collapse" aria-expanded="false"
-                           aria-controls="#task{{$projet->id}}"
-                           style="font-size:12px;">
-                            <i class="fa fa-chevron-circle-down" aria-hidden="true"></i>
-                            Voir les tâches
-                        </a>
-                                <button class="btn btn-info" style="margin-left:50px;">
-                                    {{$projet->encaisse}} €
-                                </button>
-                                <button class="btn btn-success">
-                                    {{$projet->facturable}} €
-                                </button>
-                                <button class="btn btn-primary">
-                                    {{$heures_notees}}h
-                                 </button>
-                    </span>
-                            </div>
-                            <div class="collapse" id="pr{{$projet->id}}">
-                                <p style="margin-top:25px; margin-bottom:25px;"><strong>Description
-                                        :</strong> {{ $projet->commentaire }}</p>
-                                @if($projet->etape_id > 0)
-                                    <span class="label label-success" style="font-size:10px;">
-                                 {!! $etape->etape !!} ({{round($pc_projet)}} %)
-                                        @else
-                                            <span class="label label-warning" style="font-size:10px;">
-                                {{ $etape }}
-                                                @endif
-                            </span>
-                                            <div class="progress">
-                                                <div class="progress-bar progress-bar-success progress-bar-striped"
-                                                     role="progressbar" aria-valuenow="{{$pc_projet}}" aria-valuemin="0"
-                                                     aria-valuemax="100" style="width: {{$pc_projet}}%">
-                                                </div>
-                                            </div>
-                                            <h3>Progression dans les tâches ({{ round($pc) }} %)</h3>
-                                            @if($projet->etape_id > 0)
+                        $etape = "Le projet n'a pas encore commencé";
+                        if ($projet->etape_id > 0) {
+                            $etape = \App\Etape::findOrFail($projet->etape_id);
+                        }
+                        ?>
+                                <!-- TABLEAU PROJETS -->
+                        <div style="margin-bottom: 15px;">
+                            <div class="content-panel download">
+                                <div class="text-center">
+                                    <h3 style="display: inline-block;">
+                                        <a href="#pr{{$projet->id}}"
+                                           data-toggle="collapse" aria-expanded="false"
+                                           aria-controls="#pr{{$projet->id}}">
+                                            {{ $projet->nom }}
+                                        </a>
+                                    </h3>
+                                    <a href="{{route('projet', [$projet->agence_id, $projet->id])}}"
+                                       class="btn btn-success btn-xs"
+                                       style="margin-bottom: 15px;margin-left: 20px;">Détail du projet</a>
+                                    @if($user_id == $cdp_id || $statut_id == $ca_id)
+                                        <a class="btn btn-primary btn-xs" style="margin-bottom: 15px;margin-left: 20px;"
+                                           href="#edit{{$projet->id}}" data-toggle="modal"
+                                           aria-controls="#edit{{$projet->id}}">
+                                            <i class="fa fa-pencil"></i>
+                                        </a>
+                                        <a class="btn btn-danger btn-xs" style="margin-bottom: 15px;"
+                                           href="{{ route('projet.destroy', [$agence->id, $projet->id]) }}"
+                                           data-method="delete"
+                                           data-confirm="Voulez-vous réellement supprimer ce projet ?">
+                                            <i class="fa fa-trash-o "></i>
+                                        </a>
+                                        @include('projet.edit')
+                                    @endif
+                                    <span style="margin-left:30px;">
+                            <a href="#task{{$projet->id}}"
+                               data-toggle="collapse" aria-expanded="false"
+                               aria-controls="#task{{$projet->id}}"
+                               style="font-size:12px;">
+                                <i class="fa fa-chevron-circle-down" aria-hidden="true"></i>
+                                Voir les tâches
+                            </a>
+                                    <button class="btn btn-info" style="margin-left:50px;">
+                                        {{$projet->encaisse}} €
+                                    </button>
+                                    <button class="btn btn-success">
+                                        {{$projet->facturable}} €
+                                    </button>
+                        </span>
+                                </div>
+                                <div class="collapse" id="pr{{$projet->id}}">
+                                    <p style="margin-top:25px; margin-bottom:25px;"><strong>Description
+                                            :</strong> {{ $projet->commentaire }}</p>
+                                    @if($projet->etape_id > 0)
+                                        <span class="label label-success" style="font-size:10px;">
+                                     {!! $etape->etape !!} ({{round($pc_projet)}} %)
+                                            @else
+                                                <span class="label label-warning" style="font-size:10px;">
+                                    {{ $etape }}
+                                                    @endif
+                                </span>
                                                 <div class="progress">
                                                     <div class="progress-bar progress-bar-success progress-bar-striped"
                                                          role="progressbar" aria-valuenow="{{$pc_projet}}" aria-valuemin="0"
