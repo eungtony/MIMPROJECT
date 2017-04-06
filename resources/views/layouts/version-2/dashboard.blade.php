0b8cbe3e8ced72596147ad223d185c6dc736fe91
cedricnoel <cedric10troyes@gmail.com>
2017-04-06 17:52:02 +0200

    Dashboard table is now working

--- resources/views/layouts/version-2/dashboard.blade.php
+++ resources/views/layouts/version-2/dashboard.blade.php
@@ -275,6 +275,187 @@
 
 <div class="row">
 	<div class="col-lg-12 portlets">
+		@if($projets->isEmpty())
+            <p class="alert alert-warning text-center">
+                Aucun projet n'a été crée !
+            </p>
+        @else
+
+        	@foreach ($projets as $projet)
+        		@php
+        			$taches = \App\Travail::where('projet_id', $projet->id)->orderBy('id', 'desc')->get();
+                    if (request()->only('sort')['sort'] == 'date') {
+                        $taches = \App\Travail::where('projet_id', $projet->id)->orderBy('date', 'asc')->get();
+                    } elseif (request()->only('sort')['sort'] == 'category') {
+                        $taches = \App\Travail::where('projet_id', $projet->id)->orderBy('categorie_id', 'asc')->get();
+                    } elseif (request()->only('sort')['sort'] == 'done') {
+                        $taches = \App\Travail::where('projet_id', $projet->id)->orderBy('fait', 'desc')->get();
+                    }
+                    $taches->load('user');
+                    $users = \App\User::where('agence_id', $projet->agence_id)->get();
+                    $done = \App\Travail::where('projet_id', $projet->id)->where('fait', 1)->get()->count();
+                    $total = \App\Travail::where('projet_id', $projet->id)->get()->count();
+                    $projet_heures = \App\HeuresTaches::where('projet_id', $projet->id)->get();
+                    $heures_notees = 0;
+                    foreach ($projet_heures as $heure) {
+                        $heures_notees = $heures_notees + $heure->heures;
+                    }
+                    $pc = 0;
+                    $pc_projet = 0;
+                    $heures = 0;
+
+                    if ($total_etape > 0) {
+                        $pc_projet = 100 * $projet->etape_id / $total_etape;
+                    }
+
+                    if ($total > 0) {
+                        $pc = 100 * $done / $total;
+                    }
+                    if ($projet->total_heures > 0) {
+                        $heures = 100 * $heures_notees / $projet->total_heures;
+                    }
+
+                    $etape = "Le projet n'a pas encore commencé";
+                    if ($projet->etape_id > 0) {
+                        $etape = \App\Etape::findOrFail($projet->etape_id);
+                    }
+        		@endphp
+
+				<div id="website-statistics1" class="widget">
+					<div class="widget-header transparent">
+						<h2><i class="icon-bag"></i> Projet : <strong>{{ $projet->nom }}</strong></h2>
+						<div class="additional-btn">
+							  <a class="hidden" id="dropdownMenu1" data-toggle="dropdown">
+								<i class="fa fa-plus"></i>
+							  </a>
+							  <ul class="dropdown-menu pull-right" role="menu" aria-labelledby="dropdownMenu1">
+								<li>
+									<a href="{{ route('projet', [$projet->agence_id, $projet->id]) }}">Détails du projet</a>
+								</li>
+								<li><a href="#">Editer projet</a></li>
+								<li class="divider"></li>
+								<li><a href="#">Supprimer projet</a></li>
+							  </ul>
+							 <a href="#" class="widget-popout hidden tt" title="Pop Out/In"><i class="icon-publish"></i></a>
+							<a href="#" class="widget-maximize hidden"><i class="icon-resize-full-1"></i></a>
+							<a href="#" class="widget-toggle"><i class="icon-down-open-2"></i></a>
+							<a href="#" class="widget-close"><i class="icon-cancel-3"></i></a>
+						</div>
+					</div>
+					<div class="widget-content" style="display: none;">
+						<div id="website-statistic" class="statistic-chart">	
+							<div class="row stacked">
+								<div class="col-sm-12">
+									<div class="toolbar">
+										<!-- Space for additional features -->
+									</div>
+									<div class="clearfix"></div>
+									<div style="padding: 15px;">
+										<div class="title">
+											<h4>Description du projet :</h4>
+											<p>{{ $projet->commentaire }}</p>
+											<hr>
+										</div>
+										<div>
+											<h4 class="project-title">Progression dans les tâches :  
+												<strong>
+													{{$heures_notees}}h / {{$projet->total_heures}}h - 
+                                                </strong>
+                                                <strong>
+                                                	@if (round($pc) < 50)
+                                                		<span class="text-danger">{{ round($pc) }} %</span>
+                                                	@elseif (round($pc) >= 50 && round($pc) < 75) 
+														<span class="text-warning">{{ round($pc) }} %</span>
+													@else
+														<span class="text-success">{{ round($pc) }} %</span>
+                                                	@endif
+                                                </strong>
+											</h4>
+											<hr>
+										</div>
+										<div class="title">
+											<h4>Tâches relatives au projet :</h4>
+											<hr>
+										</div>
+
+										@php
+											$taskDone = \App\Travail::where('projet_id', $projet->id)
+												->where('fait', 1)->get()->count();
+                    						$totalTask = \App\Travail::where('projet_id', $projet->id)->get()->count();
+										@endphp
+
+										@if($taskDone != $totalTask)
+										<div class="table-responsive">
+											<table data-sortable class="table">
+												<thead>
+													<tr>
+														<th>No</th>
+														<th style="width: 30px" data-sortable="false"><input type="checkbox" class="rows-check"></th>
+														<th>Description</th>
+														<th>Etat</th>
+														<th>Attribué à</th>
+														<th>Type</th>
+														<th>Délai</th>
+														<th data-sortable="false">Option</th>
+													</tr>
+												</thead>
+	                        					@foreach($taches as $tache)
+	                        						@php
+	                        							$date = \Carbon\Carbon::createFromFormat('Y-m-d', $tache->date);
+	                            						$difference = ($date->diff($now)->days < 1) ? 'today' : $date->diffInDays($now);
+	                        						@endphp
+	                        						<tr>
+														<td>{{ $tache->id }}</td>
+														<td><input type="checkbox" class="rows-check"></td>
+														<td>{{ $tache->titre }}</td>
+														<td>
+															@if($tache->fait == 0)
+					                                            <span class="label label-danger">A Faire</span>
+					                                        @else
+					                                            <span class="label label-success">Fait</span>
+					                                        @endif
+														</td>
+														<td>
+															@if($tache->user)
+				                                                <strong>{{$tache->user->name}}</strong>
+				                                            @else
+				                                                <strong>Aucune personne assignée</strong>
+				                                            @endif
+														</td>
+														<td>{{ $tache->categorie->titre }}</td>
+														<td>
+															@if ($tache->fait == 0)
+																J - {{ $difference }}
+															@else 
+																-
+															@endif
+														</td>
+														<td>
+															<div class="btn-group btn-group-xs">
+																<a data-toggle="tooltip" title="Off" class="btn btn-default"><i class="fa fa-power-off"></i></a>
+																<a data-toggle="tooltip" title="Edit" class="btn btn-default"><i class="fa fa-edit"></i></a>
+															</div>
+														</td>
+													</tr>
+	                        					@endforeach
+	                        				</table>
+	                        			</div>
+                        				@endif
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+        	@endforeach
+
+        @endif
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-12 portlets">
 		<div id="website-statistics1" class="widget">
 			<div class="widget-header transparent">
 				<h2><i class="icon-bag"></i> Projet : <strong>Solution 10</strong></h2>
