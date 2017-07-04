6f422a38a013d5ec1e260eb87c67b9dde4421d6b
Tony <eungtony@outlook.fr>
2017-07-04 15:43:53 +0200

    check if there is an active promotion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -177,25 +177,25 @@
                         </form>
                     </div>
                 </div>
-
-                @foreach($agences as $agence)
-                    <?php
-                    $cdp = 'None';
-                    $cdp_id = $agence->user_id;
-                    if ($cdp_id != 0) {
-                        $cdp = \App\User::findOrFail($cdp_id);
-                    }
-                    $encaisse = 0;
-                    $facturable = 0;
-                    foreach ($agence->projets as $projet) {
-                        $facturable = $facturable + $projet->facturable;
-                        $encaisse = $encaisse + $projet->encaisse;
-                    }
-                    ?>
-                    <div class="content-panel text-center" style="margin-bottom: 10px;">
-                        <h4>
-                            <a href="#agence{{$agence->id}}" data-parent="#accordion"
-                               data-toggle="collapse">{{$agence->nom}}</a>
+                @if(!$agences->isEmpty())
+                    @foreach($agences as $agence)
+                        <?php
+                        $cdp = 'None';
+                        $cdp_id = $agence->user_id;
+                        if ($cdp_id != 0) {
+                            $cdp = \App\User::findOrFail($cdp_id);
+                        }
+                        $encaisse = 0;
+                        $facturable = 0;
+                        foreach ($agence->projets as $projet) {
+                            $facturable = $facturable + $projet->facturable;
+                            $encaisse = $encaisse + $projet->encaisse;
+                        }
+                        ?>
+                        <div class="content-panel text-center" style="margin-bottom: 10px;">
+                            <h4>
+                                <a href="#agence{{$agence->id}}" data-parent="#accordion"
+                                   data-toggle="collapse">{{$agence->nom}}</a>
                             <span style="margin-right:30px; margin-left:30px;">
                                 Chef de projet: @if($cdp == 'None') AUCUN @else {{$cdp->name}} @endif
                             </span>
@@ -205,100 +205,106 @@
                             <span class="btn btn-info" style="margin-right:20px;">
                                 {{$facturable}} €
                             </span>
-                            <a href="{{route('agence', $agence->id)}}" class="btn btn-success btn-xs">Voir
-                                l'agence</a>
-                        </h4>
+                                <a href="{{route('agence', $agence->id)}}" class="btn btn-success btn-xs">Voir
+                                    l'agence</a>
+                            </h4>
 
-                        <div class="collapse" id="agence{{$agence->id}}">
-                            <h3>Projets</h3>
+                            <div class="collapse" id="agence{{$agence->id}}">
+                                <h3>Projets</h3>
 
-                            @if(!$agence->projets->isEmpty())
+                                @if(!$agence->projets->isEmpty())
 
-                                <table class="table">
-                                    <thead>
-                                    <th>Nom</th>
-                                    <th>Progression du projet</th>
-                                    <th>Progression des tâches</th>
-                                    <th>Devis</th>
-                                    </thead>
-                                    <tbody>
+                                    <table class="table">
+                                        <thead>
+                                        <th>Nom</th>
+                                        <th>Progression du projet</th>
+                                        <th>Progression des tâches</th>
+                                        <th>Devis</th>
+                                        </thead>
+                                        <tbody>
 
-                                    @foreach($agence->projets as $projet)
+                                        @foreach($agence->projets as $projet)
 
-                                        <?php
-                                        $done = \App\Travail::where('projet_id', $projet->id)->where('fait', 1)->get()->count();
-                                        $total = \App\Travail::where('projet_id', $projet->id)->get()->count();
-                                        $devis = \App\Devis::where('projet_id', $projet->id)->get();
-                                        if (isset($devis[0])) {
-                                            $devisModel = $devis[0];
-                                        }
-                                        if ($total_etape > 0) {
-                                            $pc_projet = 100 * $projet->etape_id / $total_etape;
-                                        }
-                                        $pc = 0;
-                                        if ($total > 0) {
-                                            $pc = 100 * $done / $total;
-                                        }
-                                        ?>
+                                            <?php
+                                            $done = \App\Travail::where('projet_id', $projet->id)->where('fait', 1)->get()->count();
+                                            $total = \App\Travail::where('projet_id', $projet->id)->get()->count();
+                                            $devis = \App\Devis::where('projet_id', $projet->id)->get();
+                                            if (isset($devis[0])) {
+                                                $devisModel = $devis[0];
+                                            }
+                                            if ($total_etape > 0) {
+                                                $pc_projet = 100 * $projet->etape_id / $total_etape;
+                                            }
+                                            $pc = 0;
+                                            if ($total > 0) {
+                                                $pc = 100 * $done / $total;
+                                            }
+                                            ?>
 
-                                        <tr>
-                                            <td>
-                                                <a href="{{route('projet', [$projet->agence_id, $projet->id])}}">{{$projet->nom}}</a>
-                                            </td>
-                                            <td>
-                                                <div class="progress">
-                                                    <div class="progress-bar progress-bar-success progress-bar-striped"
-                                                         role="progressbar" aria-valuenow="{{$pc_projet}}"
-                                                         aria-valuemin="0"
-                                                         aria-valuemax="100" style="width: {{$pc_projet}}%">
-                                                        {{round($pc_projet)}} %
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                @if($projet->etape_id > 0)
+                                            <tr>
+                                                <td>
+                                                    <a href="{{route('projet', [$projet->agence_id, $projet->id])}}">{{$projet->nom}}</a>
+                                                </td>
+                                                <td>
                                                     <div class="progress">
                                                         <div class="progress-bar progress-bar-success progress-bar-striped"
-                                                             role="progressbar" aria-valuenow="{{$pc}}"
+                                                             role="progressbar" aria-valuenow="{{$pc_projet}}"
                                                              aria-valuemin="0"
-                                                             aria-valuemax="100" style="width: {{$pc}}%">
-                                                            {{round($pc)}} %
+                                                             aria-valuemax="100" style="width: {{$pc_projet}}%">
+                                                            {{round($pc_projet)}} %
                                                         </div>
                                                     </div>
-                                                @else
-                                                    <div class="progress">
-                                                        <div class="progress-bar progress-bar-danger progress-bar-striped"
-                                                             role="progressbar" aria-valuenow="100" aria-valuemin="0"
-                                                             aria-valuemax="100" style="width: {{$pc}}%">
+                                                </td>
+                                                <td>
+                                                    @if($projet->etape_id > 0)
+                                                        <div class="progress">
+                                                            <div class="progress-bar progress-bar-success progress-bar-striped"
+                                                                 role="progressbar" aria-valuenow="{{$pc}}"
+                                                                 aria-valuemin="0"
+                                                                 aria-valuemax="100" style="width: {{$pc}}%">
+                                                                {{round($pc)}} %
+                                                            </div>
                                                         </div>
-                                                        Le projet n'a pas commencé !
-                                                    </div>
-                                                @endif
-                                            </td>
-                                            <td>
-                                                @if($devis->isEmpty())
-                                                    Aucun devis n'a été posté !
-                                                @else
-                                                    @if($devisModel->valide ==0)
-                                                        <a href="{{route('projet', [$projet->agence_id, $projet->id])}}#devis"
-                                                           class="btn btn-info btn-xs">Voir le devis</a>
                                                     @else
-                                                        <button class="btn btn-success">Devis validé</button>
+                                                        <div class="progress">
+                                                            <div class="progress-bar progress-bar-danger progress-bar-striped"
+                                                                 role="progressbar" aria-valuenow="100"
+                                                                 aria-valuemin="0"
+                                                                 aria-valuemax="100" style="width: {{$pc}}%">
+                                                            </div>
+                                                            Le projet n'a pas commencé !
+                                                        </div>
                                                     @endif
-                                                @endif
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                    @else
-                                        <p class="bg-danger">
-                                            Cette agence ne possède pas de projets !
-                                        </p>
-                                    @endif
-                                    </tbody>
-                                </table>
+                                                </td>
+                                                <td>
+                                                    @if($devis->isEmpty())
+                                                        Aucun devis n'a été posté !
+                                                    @else
+                                                        @if($devisModel->valide ==0)
+                                                            <a href="{{route('projet', [$projet->agence_id, $projet->id])}}#devis"
+                                                               class="btn btn-info btn-xs">Voir le devis</a>
+                                                        @else
+                                                            <button class="btn btn-success">Devis validé</button>
+                                                        @endif
+                                                    @endif
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        @else
+                                            <p class="bg-danger">
+                                                Cette agence ne possède pas de projets !
+                                            </p>
+                                        @endif
+                                        </tbody>
+                                    </table>
+                            </div>
                         </div>
+                    @endforeach
+                @else
+                    <div class="alert alert-warning">
+                        Aucune promotion active !
                     </div>
-                @endforeach
+                @endif
             </div>
         </div>
     </div>
